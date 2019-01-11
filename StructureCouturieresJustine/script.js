5c55f87dc74722b8e4a55b0d9d6074dd0b601fe1
justinelobert <45568541+justinelobert@users.noreply.github.com>
2019-01-11 13:03:17 +0100

    Add files via upload

---
+++ StructureCouturieresJustine/script.js
@@ -0,0 +1,7 @@
+let storm = () => {
+  console.log("hello les ptit malandrin !")
+  let logo = document.getElementById("logol");
+  logo.style.background = "blue";
+  logo.style.width = "200px";
+  logo.style.height = "200px";
+}
